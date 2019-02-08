1645def3ce5d1bb5baea2f0282f5687d9e89f238
sg <1313566+RONNCC@users.noreply.github.com>
2019-02-08 01:07:46 -0800

    Update small_setupvm.sh

--- small_setupvm.sh
+++ small_setupvm.sh
@@ -4,14 +4,6 @@
 sudo apt-get update
 sudo apt-get -y install tmux build-essential 
 
-# install bashmarks
-git clone git://github.com/huyng/bashmarks.git
-cd bashmarks
-make install
-echo "source ~/.local/bin/bashmarks.sh" >> ~/.bashrc
-cd ..
-rm -rf bashmarks
-
 ##install vim/tmux package managers
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -21,3 +13,7 @@ wget https://raw.githubusercontent.com/RONNCC/startbox/master/.vimrc
 wget https://raw.githubusercontent.com/RONNCC/startbox/master/.tmux.conf
 wget https://raw.githubusercontent.com/RONNCC/startbox/master/.gitconfig
 wget https://raw.githubusercontent.com/RONNCC/startbox/master/.bashrc
+
+# install git-completion.bash and bashmarks
+curl "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash" -o ~/.git-completion.bash
+git clone git://github.com/huyng/bashmarks.git && cd bashmarks && make install && cd .. && rm -rf bashmarks
