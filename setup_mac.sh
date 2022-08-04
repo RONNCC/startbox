fc9fe5b6db43314a46aafa4ecccfa7d6140c329c
Ronnie Ghose <1313566+RONNCC@users.noreply.github.com>
2022-08-03 19:04:36 -0700

    Update setup_mac.sh

--- setup_mac.sh
+++ setup_mac.sh
@@ -33,8 +33,8 @@ brew install dos2unix
 brew install fzf
 $(brew --prefix)/opt/fzf/install
 
-# google sheets editing
-pip install gspread
+# google sheets editing and also using pandas connector
+pip install gspread gspread-pandas
 
 
 ## Install X11 Server to allow X11 Forwarding
