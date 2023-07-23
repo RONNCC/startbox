757266585064ecf2fe4cef24ad2f2389ef36721b
Ronnie Ghose <1313566+RONNCC@users.noreply.github.com>
2023-07-23 13:34:41 -0700

    Update setup_mac.sh

--- setup_mac.sh
+++ setup_mac.sh
@@ -36,7 +36,7 @@ brew install grep coreutils findutils wget telnet
 # Mosh > SSH for intermittent connectivity ( *cough* bart)
 brew install mosh
 
-# Benchmarking tool - does multiple runs and gives average
+# Benchmarking/timing tool - does multiple runs and gives average
 brew install hyperfine
 
 # Install rclone
